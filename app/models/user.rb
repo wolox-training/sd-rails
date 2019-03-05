eea1f0511d610c8c29f6c072698edeab01c69f84
Sebas <sebasdeldi123@gmail.com>
2019-03-05 10:33:35 -0500

    Create rent model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :rents
 end
