f639debb71de0f5733afdcf4b6a196493aff7bbd
Sebas <sebasdeldi123@gmail.com>
2019-03-06 11:23:52 -0500

    Add null check in db level for book

--- db/migrate/20190304182446_create_books.rb
+++ db/migrate/20190304182446_create_books.rb
@@ -1,12 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-      t.string :genre
-      t.string :author
-      t.string :image
-      t.string :title
-      t.string :editor
-      t.string :year
+      t.string :genre, null: false
+      t.string :author, null: false
+      t.string :image, null: false
+      t.string :title, null: false
+      t.string :editor, null: false
+      t.string :year, null: false
 
       t.timestamps
     end
