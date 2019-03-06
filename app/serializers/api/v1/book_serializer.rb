c47b4c95e52ee406783609a1a966bd860fba937b
Sebas <sebasdeldi123@gmail.com>
2019-03-06 17:47:05 -0500

    Fix linting issue

--- app/serializers/api/v1/book_serializer.rb
+++ app/serializers/api/v1/book_serializer.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class BookSerializer < ActiveModel::Serializer
       attributes :id, :author, :title, :genre, :editor, :year, :image
-      has_many :rents      
+      has_many :rents
     end
   end
 end
