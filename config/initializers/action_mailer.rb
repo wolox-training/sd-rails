2481139ca0d55ac62523d3df2dbebee8d6ca017d
Sebas <sebasdeldi123@gmail.com>
2019-03-08 18:05:56 -0500

    Add port to env

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     password: Rails.application.secrets.mailer_password,
     address: Rails.application.secrets.mailer_address,
     domain: Rails.application.secrets.mailer_domain,
-    port: '2525',
+    port: Rails.application.secrets.mailer_port,
     authentication: :cram_md5
   }
 end
