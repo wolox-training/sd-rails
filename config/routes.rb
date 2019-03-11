f61f85bdaed6b2c0735616f8cd786fe935c02a42
Sebas <sebasdeldi123@gmail.com>
2019-03-11 14:49:55 -0500

    implement book suggestion endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :books, only: [:index, :show]
       resources :rents, only: [:index, :create]
+      resources :book_suggestions, only: [:create]
     end
   end
 
