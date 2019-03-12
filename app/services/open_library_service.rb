d6f4df414132ea7ebb0a62fbf76d6b773983e867
Sebas <sebasdeldi123@gmail.com>
2019-03-12 13:13:30 -0500

    fix pr comments

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -1,14 +1,43 @@
 class OpenLibraryService
   include HTTParty
+  base_uri 'https://openlibrary.org'
 
-  def book(isbn)
-    params_url = "bibkeys=ISBN:#{isbn}&format=json&jscmd=data"
-    HTTParty.get(base_uri + params_url)
+  def initialize(isbn)
+    @isbn = isbn
+    @options = { query: { bibkeys: "ISBN:#{isbn}", format: 'json', jscmd: 'data' } }
+  end
+
+  def book
+    response = self.class.get('/api/books', @options)
+    format(@isbn, response)
   end
 
   private
 
-  def base_uri
-    'http://openlibrary.org/api/books?'
+  def format(isbn, response)
+    response = JSON.parse(response.body)["ISBN:#{isbn}"]
+    if response.nil?
+      not_found(isbn)
+    else
+      found_record(isbn, response)
+    end
+  end
+
+  def not_found(isbn)
+    {
+      description: 'RECORD_NOT_FOUND',
+      message: "Record with isbn #{isbn} was not found.",
+      status: 404
+    }
+  end
+
+  def found_record(isbn, response)
+    {
+      isbn: isbn,
+      title: response['title'],
+      subtitle: response['subtitle'],
+      number_of_pages: response['number_of_pages'],
+      authors: response['authors']
+    }
   end
 end
