fc93031a5a8b2462dd34e3c52fdc60fa7ae8a49c
Sebas <sebasdeldi123@gmail.com>
2019-03-19 10:44:53 -0500

    put migration logic in rake task

--- db/migrate/20190318130015_change_book_title_length.rb
+++ db/migrate/20190318130015_change_book_title_length.rb
@@ -1,10 +1,5 @@
 class ChangeBookTitleLength < ActiveRecord::Migration[5.1]
   def change
-    Book.find_each do |book|
-      if book.title.length > 25
-        book.update(title: book.title.truncate(25))
-      end
-    end
-    change_column :books, :title, :string, :limit => 25
+    change_column :books, :title, :string, limit: 25
   end
 end
